492b16b1a744dc19dc04049584ae9f9f4fced005
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-08-29 18:58:26 +0530

    Update compute_range.h

--- compute_range.h
+++ compute_range.h
@@ -3,3 +3,5 @@
 void compute_range(int* reading, int size);
 void write_data_to_file(int head, int tail, int range);
 int get_next_reading(int index, int size, int* reading);
+void getRanges(int* reading, int size);
+void getHead(int range_count, int head, int* head_reading);
