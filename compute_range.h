2a0bb2db60dd3a28eec95b80d45c6c23bd13eba0
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-08-29 18:34:25 +0530

    removing sorting function with predefined lib function

--- compute_range.h
+++ compute_range.h
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
 void compute_range(int* reading, int size);
-void sortReading(int* reading, int size);
 void write_data_to_file(int head, int tail, int range);
 int get_next_reading(int index, int size, int* reading);
