c0aec6c0e43b74a084cd7dd785c9de4cf080195e
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-08-29 17:31:30 +0530

    Update compute_range.h

--- compute_range.h
+++ compute_range.h
@@ -2,5 +2,5 @@
 
 void compute_range(int* reading, int size);
 void sortReading(int* reading, int size);
-void write_data_to_csv(int head, int tail, int range);
+void write_data_to_file(int head, int tail, int range);
 int get_next_reading(int index, int size, int* reading);
