8cc3ecc29add7f6e6477503350fff870432cb976
YashParsai <105203897+YashParsai@users.noreply.github.com>
2022-09-05 15:45:07 +0530

    Update test.c

--- test.c
+++ test.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <string.h"
 #include "assert.h"
 
 int main()
 {
   char buffer_range[120] = {0};
   int range_reading[10] = {0,1,2,3,0,6,7,0,10,11};
-  char* actual_range_string = "1-3, 3\n6-7, 2\n10-11, 2\n"
+  char* actual_range_string = "1-3, 3\n6-7, 2\n10-11, 2\n";
    
   compute_range(buffer_range, range_reading, (sizeof(range_reading)/sizeof(int)));
   
